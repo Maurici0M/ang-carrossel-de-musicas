7d8d6cf9db6fead5033023a0bfcb0a15a92f106f
Maurici0M <mauriciomarquescosta7@gmail.com>
2024-12-24 18:38:20 -0300

    new responsive footer added

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  lastVersionUpdate: string = 'V1.2.0';
+  lastUpdate: string = 'Atualizada em 22/12/2024';
+
+  ngOnInit(): void {
+    console.log("Versão atual: ", this.lastVersionUpdate, this.lastUpdate)
+  }
 
 }
